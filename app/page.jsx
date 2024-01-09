f0a3d1e2da4159347d237c1d9d1a20ffe3939d81
Vasanthmobiux <vasanth@mobiux.in>
2024-01-09 16:53:02 +0530

    create-prompt api

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
+import Feed from "@components/Feed";
+
 const Home = () => (
   <section className="w-full flex-center flex-col ">
-    <h1 className="head_text text-center">
+    <h1 className="head_text text-center ">
       Discover & Share
       <br className="max-md:hidden" />
       <span className="orange_gradient text-center">AI-Powered Prompts</span>
@@ -9,6 +11,7 @@ const Home = () => (
       Promptspark is an open-source AI prompting tool for modern world to
       discover,create and share creative prompts.
     </p>
+    <Feed />
   </section>
 );
 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = () => {
-  return <div>PromptCard</div>;
+const PromptCard = ({ post, handleTagClick }) => {
+  const [copied, setcopied] = useState("");
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={post.creator.image}
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+            alt="user_image"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="">{post.creator.email}</p>
+          </div>
+        </div>
+        <div className="copy_btn">
+          <Image
+            src={
+              copied === post.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
+            }
+            width={12}
+            height={12}
+          />
+        </div>
+      </div>
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="cursor-pointer blue_gradient text-sm font-inter"
+        onClick={() => {
+          handleTagClick && handleTagClick(post);
+        }}
+      >
+        {post.tag}
+      </p>
+    </div>
+  );
 };
 
 export default PromptCard;
